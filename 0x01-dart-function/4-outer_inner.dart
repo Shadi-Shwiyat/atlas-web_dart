78f552ae498f0b5901397a60fee7b1d389de565e
Shadi-Shwiyat <raz611103@gmail.com>
2024-06-14 17:07:29 -0500

    task 4 outer and inner functions

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -7,7 +7,7 @@ void outer(String name, String id) {
   if (spaceIndex != -1) {
     String lastInitial = name.substring(spaceIndex + 1, spaceIndex + 2);
     String firstName = name.substring(0, spaceIndex);
-    print('${inner()} $lastInitial.$firstName your id is 001');
+    print('${inner()} $lastInitial.$firstName your id is $id');
   } else {
     print("NO SPACES IN NAMEEEE");
   }
