5d6760f17975c460fdb08af31f356f2dce8fa2e3
Shadi-Shwiyat <raz611103@gmail.com>
2024-06-14 17:06:52 -0500

    task 4 outer and inner functions

---
+++ 0x01-dart-function/4-outer_inner.dart
@@ -0,0 +1,15 @@
+void outer(String name, String id) {
+  String inner() {
+    return "Hello Agent";
+  }
+
+  int spaceIndex = name.indexOf(' ');
+  if (spaceIndex != -1) {
+    String lastInitial = name.substring(spaceIndex + 1, spaceIndex + 2);
+    String firstName = name.substring(0, spaceIndex);
+    print('${inner()} $lastInitial.$firstName your id is 001');
+  } else {
+    print("NO SPACES IN NAMEEEE");
+  }
+
+}
