693f8cf1ff1d714923b02218a59ba527944c2e34
Shadi-Shwiyat <raz611103@gmail.com>
2024-06-16 10:58:13 -0500

    task 0 using given main file to incorrectly write a class because the main file is incorrectly written

---
+++ 0x02-dart-classes/0-class.dart
@@ -0,0 +1,13 @@
+class User {
+  String? name;
+
+  // Constructor
+  // User(String name) {
+  //   this.name = name;
+  // }
+
+  // Methods
+  String showName() {
+    return 'Hello ${name}';
+  }
+}
