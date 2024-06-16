49915ae74234c219e9b54fd9c33e1e0815dce986
Shadi-Shwiyat <raz611103@gmail.com>
2024-06-16 12:23:50 -0500

    task 5 creating user from json input

--- 0x02-dart-classes/5-json_to_user.dart
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -31,6 +31,6 @@ class User {
   }
 
   String toString() {
-    return 'User(id : ${id},name: ${name}, age: ${age}, height: ${height})';
+    return 'User(id : ${id} ,name: ${name}, age: ${age}, height: ${height})';
   }
 }
