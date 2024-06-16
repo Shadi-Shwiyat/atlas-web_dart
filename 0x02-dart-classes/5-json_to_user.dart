e502b4890cc65b6a70568d4b978192127306e8ff
Shadi-Shwiyat <raz611103@gmail.com>
2024-06-16 12:23:17 -0500

    task 5 creating user from json input

---
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -0,0 +1,36 @@
+class User {
+  int? id;
+  String? name;
+  int? age;
+  double? height;
+
+  // Constructor
+  User({this.id, this.name, this.age, this.height});
+
+  // Methods
+  String showName() {
+    return 'Hello ${name}';
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'age': age,
+      'height': height,
+    };
+  }
+
+  static User fromJson(Map<dynamic, dynamic> userJson) {
+    return User(
+      id: userJson['id'],
+      name: userJson['name'],
+      age: userJson['age'],
+      height: userJson['height'],
+    );
+  }
+
+  String toString() {
+    return 'User(id : ${id},name: ${name}, age: ${age}, height: ${height})';
+  }
+}
