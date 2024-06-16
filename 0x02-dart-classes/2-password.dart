0720fd1c942dc5d1dc782086cfcb18e9382f8dd6
Shadi-Shwiyat <raz611103@gmail.com>
2024-06-16 11:39:39 -0500

    task 2 creating a password class

---
+++ 0x02-dart-classes/2-password.dart
@@ -0,0 +1,21 @@
+class Password {
+  String password = '';
+
+  // Contructor
+  // Password(this.password);
+
+  // Methods
+  bool isValid() {
+    if (password.length < 8 || password.length > 16) {
+      return false;
+    } else if (!password.contains(RegExp(r'[A-Z]')) || !password.contains(RegExp(r'[a-z]')) || !password.contains(RegExp(r'[0-9]'))) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  String toString() {
+    return 'Your Password is: ${password}';
+  }
+}
